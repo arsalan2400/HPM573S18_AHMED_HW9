7e642f8f0a59079ac0a7838d87ce1c64b34183ca
Arsalan Ahmed <35544234+arsalan2400@users.noreply.github.com>
2018-04-10 11:42:53 -0400

    Update HW9-3.py

--- HW9-3.py
+++ HW9-3.py
@@ -22,17 +22,11 @@ def print_outcomes(simOutput, therapy_name):
     :param simOutput: output of a simulated cohort
     :param therapy_name: the name of the selected therapy
     """
-    simOutputs = cohort.simulate()
     # mean and confidence interval text of patient survival time
     survival_mean_CI_text = F.format_estimate_interval(
         estimate=simOutput.get_sumStat_survival_times().get_mean(),
         interval=simOutput.get_sumStat_survival_times().get_t_CI(alpha=Settings.ALPHA),
         deci=2)
-
-# Find the mean and confidence interval text of patient survival time. From SupportMarkovModel.py
-survival_mean_CI_text = F.format_estimate_interval(
-        estimate=simOutput.get_sumStat_survival_times().get_mean(),
-        interval=simOutput.get_sumStat_survival_times().get_t_CI(alpha=Settings.ALPHA),
-        deci=2)
-print("The mean survival time with a {:.{prec}%} confidence interval without drug is....".format(1 - Data.ALPHA, prec=0),
+    print(therapy_name)
+    print("  Estimate of mean survival time and {:.{prec}%} confidence interval:".format(1 - Settings.ALPHA, prec=0),
           survival_mean_CI_text)
